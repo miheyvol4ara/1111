3192dcab75f4bbbe7c9b22b8f682865c99967531
miheyvol4ara <vol4ara2012@mail.ru>
2012-12-16 09:44:59 +0200

    rw
    rwe

--- ¦-¦-¦¬¦+/Form2.cs
+++ ¦-¦-¦¬¦+/Form2.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+namespace балд
+{
+    public partial class Form2 : Form
+    {
+         using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
